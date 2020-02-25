50aa8fbf2846612fed027d8449dc90228bad7440
ywb <734892981@qq.com>
2020-02-25 18:01:03 +0800

    storage封装

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script>
-
+import storage from './storage'
 export default {
   name: 'App',
   components: {
@@ -16,6 +16,11 @@ export default {
   
     }
   },
+  mounted(){
+    // storage.setItem('a',1);
+    // storage.setItem('user',{a:1});
+    storage.setItem('abc',{a:1},'user');
+  }
   
 }
 </script>
--- src/pages/alipay.vue
+++ src/pages/alipay.vue
@@ -7,4 +7,4 @@
 export default {
     name:'alipay'
 }
-</script> 
+</script>  
