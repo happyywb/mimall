edf8dbd91207b53a26dd2f705daa7b18cad6caba
ywb <734892981@qq.com>
2020-02-27 11:49:26 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import router from './router'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+//根据前端跨域方式做调整
+axios.defaults.baseURL = '/api ';
+axios.defaults.timeout = 8000;
+//接口错误拦截
+axios.interceptors.response.use(function(response){
+   let res = response.data;
+   if(res.status == 0){
+     return res.data;
+   }else if(res.status == 10){
+      window.localion.href = '/#/login';
+   }else{
+     alert(res.msg);
+   }
+}) ;
+
+Vue.use(VueAxios,axios);
 Vue.config.productionTip = false
 
 new Vue({
