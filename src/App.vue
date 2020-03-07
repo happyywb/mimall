c8ea4107f9c559fbe4e29a77665e3a515b90eb53
ywb <734892981@qq.com>
2020-03-07 23:18:28 +0800

    3.7

--- src/App.vue
+++ src/App.vue
@@ -5,33 +5,39 @@
 </template>
 
 <script>
-import storage from './storage'
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-
+    
   },
   data(){
     return {
-  
+      
     }
   },
   mounted(){
-    // storage.setItem('a',1);
-    // storage.setItem('user',{a:1});
-    // storage.setItem('abc',{a:1},'user');
+    if(this.$cookie.get('userId')){
+      this.getUser();
+      this.getCartCount();
+    }
+  },
+  methods:{
+    getUser(){
+      this.axios.get('/user').then((res={})=>{
+        this.$store.dispatch('saveUserName',res.username);
+      })
+    },
+    getCartCount(){
+      this.axios.get('/carts/products/sum').then((res=0)=>{
+        this.$store.dispatch('saveCartCount',res);
+      })
+    }
   }
-  
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import './assets/scss/reset.scss';
+@import './assets/scss/config.scss';
+@import './assets/scss/button.scss';
 </style>
